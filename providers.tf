961e52d6ffb2a551c96b2d7dd1c6ed2178039618
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-07-07 17:32:36 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -3,12 +3,3 @@ provider "aws" {
   profile = "ecr-eks-user1"
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "lili-terraform-state"
-    key            = "terraform/state/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state"
-    encrypt        = true
-  }
-}
